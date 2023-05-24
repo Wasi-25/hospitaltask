fc3d71f6c8e08e2639e1290a3bca37446959500d
Wasiullah Khalique <79196849+Wasi-25@users.noreply.github.com>
2023-05-24 21:51:56 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const path = require("path");
 
 require("dotenv").config({"path":"backend/config/config.env"});
 
-app.use(cors())
 app.use(express.json());
 app.use("/api/", require("./routes/PatientRoute"));
 app.use("/api/", require("./routes/ServiceRoute"));
 app.use("/api/", require("./routes/BillRoute"));
+
+if(process.env.NODE_ENV!=="production"){
+    require("dotenv").config({path: "./config/config.env"});
+}
+
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
+
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
