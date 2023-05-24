531987a3f91221f806791521a80b9461b46bb97e
Wasiullah Khalique <79196849+Wasi-25@users.noreply.github.com>
2023-05-24 14:44:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config({"path":"backend/config/config.env"});
 
@@ -10,8 +11,10 @@ app.use("/api/", require("./routes/PatientRoute"));
 app.use("/api/", require("./routes/ServiceRoute"));
 app.use("/api/", require("./routes/BillRoute"));
 
-app.get("/",(req,res)=>{
-    res.send("Working...")
-})
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "../build/index.html"));
+});
 
 module.exports = app;
