d0af93e844c49bd67fe0a08882f1193432f47034
Wasiullah Khalique <79196849+Wasi-25@users.noreply.github.com>
2023-05-24 14:51:22 +0530

    Update2 app.js

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,14 @@ app.use("/api/", require("./routes/PatientRoute"));
 app.use("/api/", require("./routes/ServiceRoute"));
 app.use("/api/", require("./routes/BillRoute"));
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "../build/index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 module.exports = app;
